84ff480e6f5528b64ab3075f06c5a99fd7f9bfb6
Eduardo Barajas <ebarajas@optaresolutions.com>
2021-02-22 08:07:22 +0100

    Add functions.go

---
+++ registry/functions.go
@@ -0,0 +1,17 @@
+package registry
+
+func (*registry Registry) GetImages (map[string][]string, error) {
+	repositories, err := registry.Repositories()
+	if err != nil {
+		return nil, err
+	}
+	imageList := make(map[string][]string)
+	for _, image := range repositories {
+		tags, err := registry.Tags(image)
+		if err != nil {
+			return nil, err
+		}
+		imageList[image] = tags
+	}
+	return imageList, err
+}
